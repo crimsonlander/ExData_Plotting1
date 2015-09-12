edb757b626da2e07f0f515dfd158af3a8f4ef3b2
Denis Kuzminykh <reaper.of.midnight@gmail.com>
2015-09-12 19:38:36 +0300

    Minor fixes

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 source("read_data.R")
 
 png("plot2.png", bg="transparent")
-par(lwd = 1.3)
+par(lwd = 1)
 with(d, plot(datetime, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = ""))
 dev.off()
